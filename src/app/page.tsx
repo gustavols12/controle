a86cf10483f7279147c1b7395072278c5761610a
gustavo <gustavols2100@gmail.com>
2025-07-22 21:30:09 -0300

    Feat: Cria componente container e página home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from 'next/image';
+import Image from "next/image";
+import hero from "@/assets/hero.svg";
 
 export default function Home() {
   return (
-    <div>
-      <h1>teste</h1>
-    </div>
+    <main className="flex items-center flex-col justify-center min-h-[calc(100vh-80px)] ">
+      <h2 className="font-medium text-2xl mb-2">Gerencie sua empresa</h2>
+      <h1 className="font-bold text-3xl text-blue-500 mb-8 md:text-4xl">
+        Atendimentos e clientes
+      </h1>
+      <Image
+        src={hero}
+        alt="imagem hero"
+        width={600}
+        className="max-w-sm md:max-w-xl"
+      />
+    </main>
   );
 }
