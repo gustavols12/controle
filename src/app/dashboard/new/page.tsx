31aea6a2f34d845db48bc3dfb3877823b42856e6
gustavo <gustavols2100@gmail.com>
2025-08-01 22:02:41 -0300

    Feat: cria página de novo chamado

--- src/app/dashboard/new/page.tsx
+++ src/app/dashboard/new/page.tsx
@@ -1,7 +1,21 @@
 import { Container } from "@/components/container";
+import { authOptions } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function NewTicket() {
+export default async function NewTicket() {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) redirect("/");
+
+  const customers = await prisma.customer.findMany({
+    where: {
+      userId: session.user.id,
+    },
+  });
+
   return (
     <Container>
       <main className="mt-9 mb-2">
@@ -29,12 +43,33 @@ export default function NewTicket() {
             className="w-full border-2 rounded-md px-2 mb-2 h-24 resize-none"
             placeholder="Descreva o problema"
           ></textarea>
-          <label className="mb-1 font-medium text-lg">
-            Selecione o cliente
-          </label>
-          <select className="w-full border-2 rounded-md px-2 mb-2 h-11 bg-white">
-            <option value="cliente 1">cliente 1</option>
-          </select>
+          {customers.length > 0 && (
+            <>
+              <label className="mb-1 font-medium text-lg">
+                Selecione o cliente
+              </label>
+              <select className="w-full border-2 rounded-md px-2 mb-2 h-11 bg-white">
+                {customers.map((customer) => (
+                  <option key={customer.id} value={customer.id}>
+                    {customer.name}
+                  </option>
+                ))}
+              </select>
+            </>
+          )}
+
+          {customers.length === 0 && (
+            <Link href="/dashboard/customer/new">
+              Você não possui nenhum cliente cadastrado,{" "}
+              <span className="text-blue-500 font-medium">Cadastrar</span>
+            </Link>
+          )}
+          <button
+            className="bg-blue-500 text-white font-bold px-2 h-11 rounded-md my-4 disabled:bg-gray-400 disabled:cursor-not-allowed"
+            disabled={customers.length === 0}
+          >
+            Cadastrar
+          </button>
         </form>
       </main>
     </Container>
